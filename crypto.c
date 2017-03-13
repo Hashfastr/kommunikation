7a204eff335dd311ef7dd4e7d4c6bb796109016e
Hashfastr <robotsguy@gmail.com>
2017-03-13 13:46:13 -0600

    Create crypto.c

---
+++ crypto.c
@@ -0,0 +1,13 @@
+#include "headers"
+
+char *encrypt(char *message, char *key) {
+    char *encrypted;
+
+return encrypted;
+}
+
+char *decrypt(char *message, char *key) {
+    char *decrypted;
+
+return decrypted;
+}
